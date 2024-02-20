4ca6a79512bec84c740daadd602a74cd5e78c3f3
airmulk <airmulk@gmail.com>
2024-02-20 20:09:23 +0500

    sfdsdf

--- uni_messaging/__main__.py
+++ uni_messaging/__main__.py
@@ -1,17 +1,6 @@
 from __future__ import annotations
 
-
-def main(argv: Sequence[str] | None = None) -> int:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("filenames", nargs="*", help="Filenames to fix")
-    args = parser.parse_args(argv)
-
-    retv = 0
-
-    for filename in args.filenames:
-        pass
-
-    return retv
+from uni_messaging._main import main
 
 
 if __name__ == "__main__":
---
+++ uni_messaging/_main.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", nargs="*", help="Filenames to fix")
+    args = parser.parse_args(argv)
+
+    retv = 0
+
+    for filename in args.filenames:
+        pass
+
+    return retv
+
+if __name__ == "__main__":
+    raise SystemExit(main())
