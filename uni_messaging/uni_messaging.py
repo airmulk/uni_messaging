1e25f4788e63c11333f917e431b101f2db949903
airmulk <airmulk@gmail.com>
2024-02-20 19:15:22 +0500

    initial

---
+++ uni_messaging/uni_messaging.py
@@ -0,0 +1,15 @@
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*', help='Filenames to fix')
+    args = parser.parse_args(argv)
+
+    retv = 0
+
+    for filename in args.filenames:
+        pass
+
+    return retv
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
